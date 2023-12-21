c751466ec2093c5008a0c368279c492578621205
Gus Sánchez <goosethedev@proton.me>
2023-12-21 19:46:39 -0400

    Fixed problems with fstab partitions

--- modules/fstab/dashbox.nix
+++ modules/fstab/dashbox.nix
@@ -8,8 +8,10 @@
 
   # Fstab mount Data partition
   fileSystems."/media/Data" = {
-    device = "/dev/sda3";
-    fsType = "auto";
-    options = [ "defaults" "utf8" "umask=000" "dmask=0027" "fmask=137" "gid=1000" "uid=1000" ];
+    device = "/dev/disk/by-uuid/ff23f419-8f99-4f08-97b3-21b638d6c19c";
+    # ext4 partitions don't need any of this
+    # it's enough to change permissions and ownership in the partition files themselves
+    # fsType = "ext4";
+    # options = [ "defaults" "umask=000" "gid=100" "uid=1000" "nofail" ];
   };
 }
--- modules/fstab/home-folder-binds.nix
+++ modules/fstab/home-folder-binds.nix
@@ -6,25 +6,25 @@
   fileSystems."/home/goose/Books" = {
     device = "/media/Data/Books";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/.password-store" = {
     device = "/media/Data/Pass";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/Personal" = {
     device = "/media/Data/Personal";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/Projects" = {
     device = "/media/Data/Projects";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   # LOCAL - Not synced with Syncthing
@@ -32,30 +32,30 @@
   fileSystems."/home/goose/Desktop" = {
     device = "/media/Data/Desktop";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/Documents" = {
     device = "/media/Data/Documents";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/Downloads" = {
     device = "/media/Data/Downloads";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 
   fileSystems."/home/goose/Pictures" = {
     device = "/media/Data/Pictures";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
   
   fileSystems."/home/goose/Temp" = {
     device = "/media/Data/Temp";
     fsType = "none";
-    options = [ "defaults" "bind" ];
+    options = [ "defaults" "bind" "nofail" ];
   };
 }
