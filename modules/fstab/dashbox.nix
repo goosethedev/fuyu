cd75442db07c278a07df3d1ea66b392d17eb362c
Gus Sánchez <goosethedev@proton.me>
2023-12-21 15:22:54 -0400

    Changed /media/Data mount type to auto

--- modules/fstab/dashbox.nix
+++ modules/fstab/dashbox.nix
@@ -9,7 +9,7 @@
   # Fstab mount Data partition
   fileSystems."/media/Data" = {
     device = "/dev/sda3";
-    fsType = "ext4";
+    fsType = "auto";
     options = [ "defaults" "utf8" "umask=000" "dmask=0027" "fmask=137" "gid=1000" "uid=1000" ];
   };
 }
