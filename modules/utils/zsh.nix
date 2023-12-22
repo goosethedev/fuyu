6340c824550eb61e001fd5ed10b5df7e44a679ab
Gus Sánchez <goosethedev@proton.me>
2023-12-21 20:42:05 -0400

    Moved ZSH config to HM

--- modules/utils/zsh.nix
+++ modules/utils/zsh.nix
@@ -1,25 +1,29 @@
 { config, pkgs, ...}:
 
 {
-  # Enable ZSH
-  programs.zsh = {
-    enable = true;
-    histFile = "$HOME/.cache/.zsh_history";
-    enableCompletion = true;
-    enableGlobalCompInit = false;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    interactiveShellInit = ''
-      ZSH_COMPDUMP=$HOME/.cache/.zcompdump-$HOST
-    '';
-  };
-
+  # Basic config only. The rest is on Home Manager
+  programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ]; 
-  programs.zsh.ohMyZsh = {
-    enable = true;
-    plugins = [ "aliases" "git" ];
-    theme = "powerlevel10k/powerlevel10k";
-    custom = "$HOME/.config/zsh/oh-my-zsh";
-  };
+
+  # programs.zsh = {
+  #   enable = true;
+  #   histFile = "$HOME/.cache/.zsh_history";
+  #   enableCompletion = true;
+  #   enableGlobalCompInit = false;
+  #   autosuggestions.enable = true;
+  #   syntaxHighlighting.enable = true;
+  #   interactiveShellInit = ''
+  #     ZSH_COMPDUMP=$HOME/.cache/.zcompdump-$HOST
+  #   '';
+  # };
+
+  # users.defaultUserShell = pkgs.zsh;
+  # environment.shells = with pkgs; [ zsh ]; 
+  # programs.zsh.ohMyZsh = {
+  #   enable = true;
+  #   plugins = [ "aliases" "git" ];
+  #   theme = "powerlevel10k/powerlevel10k";
+  #   custom = "$HOME/.config/zsh/oh-my-zsh";
+  # };
 }
