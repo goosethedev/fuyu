850cde894162636c0aa6480232fe858752afe507
Gus Sánchez <goosethedev@proton.me>
2023-12-26 18:03:50 -0400

    Nvidia working on Daydream :')'

--- modules/hyprland/nvidia.nix
+++ modules/hyprland/nvidia.nix
@@ -4,22 +4,48 @@
   # Enable Hyprland
   programs.hyprland = {
     enable = true;
-    enableNvidiaPatches = true;
+    # No longer necessary
+    # enableNvidiaPatches = true;
     xwayland.enable = true;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
+
+  # Enable portals (Hyprland and GTK)
+  # GTK necessary for File Picker, recommended by Hyprland Wiki
+  # https://wiki.hyprland.org/Useful-Utilities/Hyprland-desktop-portal/
+  xdg.portal = {
+    xdgOpenUsePortal = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 
   # Might possibly be needed
   environment.sessionVariables = {
     # If cursor is invisible
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint Electron apps to use Wayland
-    NIXOS_OZONE_WL = "1";
+    # This broke Electron last time. Do not enable.
+    # NIXOS_OZONE_WL = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
   };
+
+  # Extra packages recommended by wiki
+  environment.systemPackages = with pkgs; [
+    libva
+    libsForQt5.qt5ct
+    libsForQt5.qt5.qtwayland
+  ];
 
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+
+    # Fix crashes on Electron apps. I don't think does something
+    # extraPackages = [ pkgs.nvidia-vaapi-driver ];
   };
 
   hardware.nvidia = {
